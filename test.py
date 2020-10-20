edf8da0bbca18f1391fc9d36451663c4409a6f85
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-10-20 09:12:03 +0100

    ignore

--- test.py
+++ test.py
@@ -4,11 +4,13 @@ from PIL import Image
 import numpy as np
 import imutils
 
-img = cv2.imread('test.png')
+img = cv2.imread('images/test1.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-gray = cv2.bilateralFilter(gray, 11, 17, 17) 
-blurred= cv2.medianBlur(gray,5)
+norm_image = cv2.normalize(gray, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
+grayed = cv2.bilateralFilter(gray, 11, 17, 17) 
+blurred= cv2.medianBlur(grayed,5)
+rotated = cv2.rotate(blurred, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 #edged = cv2.Canny(blurred, 30, 200)
 
@@ -21,6 +23,7 @@ blurred= cv2.medianBlur(gray,5)
 #new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
 #new_image = cv2.bitwise_and(image,image,mask=mask)
 
-original = pytesseract.image_to_string(blurred, config='')
+
+original = pytesseract.image_to_string(rotated, config='')
 
 print (original)
