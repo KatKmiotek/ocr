ce3d56b1aa675747ce3a3b1de8d69f852a20e340
KatKmiotek <katarzyna.kmiotek@yahoo.com>
2020-10-20 16:32:31 +0100

    hello works

--- ocr.py
+++ ocr.py
@@ -1,12 +1,11 @@
 import cv2
 import pytesseract
 from PIL import Image
-
-
-img = cv2.imread('images/note.png', 0)
-img_rotated = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
-cv2.imshow('', img)
-img2gray = cv2.cvtColor(img_rotated, cv2.COLOR_BGR2GRAY)
+image = cv2.imread('images/note.png', 0)
+def preprocess(image)
+    #cv2.imread('images/note.png', 0)
+    cv2.rotate(imgage, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    cv2.cvtColor(img_rotated, cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(img2gray, 180, 255, cv2.THRESH_BINARY)
 image_final = cv2.bitwise_and(img2gray, img2gray, mask=mask)
 ret, new_img = cv2.threshold(image_final, 180, 255, cv2.THRESH_BINARY)
--- test.py
+++ test.py
@@ -4,22 +4,33 @@ from PIL import Image
 import numpy as np
 import imutils
 
-img = cv2.imread('images/note.png')
+img = cv2.imread('images/hello.png')
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 #cv2.imshow('image', gray)
 #cv2.waitKey(0)
 
-grayed = cv2.bilateralFilter(gray, 11, 17, 17)
+img_blur = cv2.GaussianBlur(gray,(5,5),0)
+cv2.imwrite(r"./images/result/img_blur.png", img_blur)
+
+grayed = cv2.bilateralFilter(img_blur, 11, 17, 17)
+cv2.imwrite(r"./images/result/img_grayed.png",grayed)
+
+#cv2.imshow('tres', img_blur)
+#cv2.waitKey(0)
 #cv2.imshow('image greyed', grayed)
 #cv2.waitKey(0)
-blurred= cv2.medianBlur(grayed,5)
-#cv2.imshow('image blurred', blurred)
+
+
+tres = cv2.threshold(grayed, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+cv2.imwrite(r"./images/result/img_threshold.png",tres)
+#cv2.imshow('tres', tres)
 #cv2.waitKey(0)
-rotated = cv2.rotate(blurred, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+rotated = cv2.rotate(tres, cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 custom_config= r'--oem 3 --psm 6'
 
-original = pytesseract.image_to_string(rotated, config=custom_config, lang='eng')
+original = pytesseract.image_to_string(rotated, config=custom_config)
 
 print (original)
